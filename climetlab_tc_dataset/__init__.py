569ef5cd754bdcecbb26b1e3415d43c80d2b811c
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-11-01 10:44:50 +0000

    Dataset matching to labels

--- climetlab_tc_dataset/__init__.py
+++ climetlab_tc_dataset/__init__.py
@@ -9,15 +9,56 @@
 
 
 from climetlab import Dataset, load_source
+
+from os import path
 import numpy as np
+import pandas as pd
+import datetime
 
 
 def normalise_01(a):
     return (a - np.amin(a)) / (np.amax(a) - np.amin(a))
 
 
-# retrieve, param=clbt, channel=9, date=20160401/to/20181231, ident=57, instrument=207, step=0/6, time=0/12, type=ssd, grid=0.1/0.1
-# retrieve, param=tcw
+class Labels:
+    def __init__(self, filename):
+        self.df = pd.read_csv(path.join(path.dirname(__file__), filename))
+
+        # keep only: DateTime, lat_p, lon_p, pres
+        self.df["DateTime"] = pd.to_datetime(self.df["DateTime"])
+        self.df.drop(
+            inplace=True, columns=["wind", "lat_w", "lon_w", "step", "windrad", "name"]
+        )
+
+    def datetime_min(self):
+        return min(self.df["DateTime"])
+
+    def datetime_max(self):
+        return max(self.df["DateTime"])
+
+    @staticmethod
+    def _class(p):
+        if p < 965:
+            return 3
+        if p < 990:
+            return 2
+        if p < 1005:
+            return 1
+        return 0
+
+    def label(self, date):
+        l = []
+        for row in self.df[self.df["DateTime"] == date].itertuples():
+            l.append(
+                {"lat": row.lat_p, "lon": row.lon_p, "class": self._class(row.pres)}
+            )
+        return l
+
+
+# labels file
+labels = Labels("tc_an.csv")
+date = "{}/to/{}".format(labels.datetime_min().date(), labels.datetime_max().date())
+print("labels: date={}".format(date))
 
 
 class SimSat(Dataset):
@@ -27,21 +68,37 @@ class SimSat(Dataset):
 
     def __init__(self, **req):
 
-        source = load_source("mars", param="2t", levtype="sfc", date="-1")
+        # retrieve, param=tcw
+        source = load_source(
+            "mars",
+            param="clbt",
+            date=date,
+            time=[0, 12],
+            step=[0, 6],
+            grid=[0.1, 0.1],
+            type="ssd",
+            channel="9",
+            ident="57",
+            instrument="207",
+        )
         self.source = source
 
         self._fields = []
-        for s in source:
-            self._fields.append((s, "FIXME_label_from_csv"))
+        for c, s in enumerate(source):
+            d: datetime = s.datetime()
+            if c % 2 is not 0:
+                d += datetime.timedelta(hours=6)
+            self._fields.append((s, labels.label(d)))
 
     def fields(self):
         return self._fields
 
     def title(self, label):
-        return "Cloudy brightness temperature"  # "Total column water"
+        # return "Total column water"
+        return "Cloudy brightness temperature"
 
     # load_data is used by keras
-    def load_data(self, normalise=True, test_size=0.5, shuffle=False, fields=False):
+    def load_data(self, normalise=True, test_size=0.74, shuffle=False, fields=False):
         data = []
         for field, label in self._fields:
             if normalise:
@@ -51,7 +108,7 @@ class SimSat(Dataset):
             data.append((array, label, field))
 
         if shuffle:
-            raise ValueError("This dataset does not support shuffle.")
+            raise ValueError("This dataset does not support shuffle (intentionally).")
         half = int(len(data) * (1.0 - test_size))
 
         x_train, y_train, f_train = (
